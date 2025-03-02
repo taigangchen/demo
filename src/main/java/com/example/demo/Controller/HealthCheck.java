588ce115783115f5e1866b9a8bae41333c672216
taigang.chen <tg.chen@datsch.com.cn>
2025-03-02 13:19:24 +0800

    add server port

--- src/main/java/com/example/demo/Controller/HealthCheck.java
+++ src/main/java/com/example/demo/Controller/HealthCheck.java
@@ -1,2 +1,13 @@
-package com.example.demo.Controller;public class HealthCheck {
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "200 OK!";
+    }
 }
